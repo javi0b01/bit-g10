88899d14d0a8cf2c7067f6c6bdbeb5321200e01c
javi <55107141+javi0b01@users.noreply.github.com>
2025-04-23 20:04:33 -0500

    lesson javascript

--- lesson-javascript/js/script.js
+++ lesson-javascript/js/script.js
@@ -25,8 +25,7 @@ const nombreVariable4 = 'usando "const"';
 console.log(nombreVariable4);
 */
 
-/* OPERADORES */
-// Asignación
+// Interpolación de variables
 
 /* TIPOS DE DATO PRIMITIVOS */
 // undefined
@@ -47,7 +46,6 @@ miVariable = 0x01;
 // boolean
 miVariable = true;
 miVariable = false;
-console.log('miVariable:', miVariable);
 
 /*
 Crear 3 constantes
@@ -57,10 +55,6 @@ Crear 3 constantes
 imprima en la consola su nombre completo y edad usando los valores de las constantes
 Ej de salida: Soy Pepita Pérez y tengo 32 años.
 
-- cumple
-- cumple parcialmente
-- no cumple
-*/
 const nombre = 'Pepita';
 const apellido = 'Pérez';
 const edad = 32;
@@ -68,3 +62,52 @@ console.log('Soy Pepita Pérez y tengo 32 años.');
 console.log('Soy', nombre, apellido, 'y tengo', edad, 'años.');
 console.log('Soy ' + nombre + ' ' + apellido + ' y tengo ' + edad + ' años.');
 console.log(`Soy ${nombre} ${apellido} y tengo ${edad} años.`);
+*/
+
+/* OPERADORES */
+// Asignación
+let miVariable1 = 'Pepita';
+let miVariable2 = 'Pérez';
+
+// Concatenación
+const nombreCompleto = miVariable1 + miVariable2;
+// console.log('nombreCompleto:', nombreCompleto);
+miVariable2 = 5;
+let resultado = miVariable1 + miVariable2;
+
+// Aritméticos
+miVariable1 = 10;
+resultado = miVariable1 + miVariable2;
+resultado = miVariable1 - miVariable2;
+resultado = miVariable1 * miVariable2;
+resultado = miVariable1 / miVariable2;
+resultado = miVariable1 % miVariable2;
+resultado = miVariable1 ** miVariable2;
+
+/*
+- Cree dos variables de tipo constante y asigne los valores 20 y 3 respectivamente
+- Cree una constante para guardar el resultado de cada una de las operaciones básicas (suma, resta, multiplicación y división)
+- Imprima el resultado de cada operación con el siguiente formato de ejemplo:
+20 + 3 = 23
+const num1 = 10;
+const num2 = 5;
+
+const sumResult = num1 + num2;
+const subResult = num1 - num2;
+const mulResult = num1 * num2;
+const divResult = num1 / num2;
+
+const result = `
+${num1} + ${num2} = ${sumResult}
+${num1} - ${num2} = ${subResult}
+${num1} x ${num2} = ${mulResult}
+${num1} / ${num2} = ${divResult}
+`;
+
+console.log(result);
+*/
+
+// Precedencia de operadores Ej: 12 / 3 + 2 * 3 - 1
+// PEMDAS | Paréntesis Exponentes Multiplicación División Adición Sustracción
+/* resultado = 12 / 3 + 2 * 3 - 1;
+console.log('resultado:', resultado); */
