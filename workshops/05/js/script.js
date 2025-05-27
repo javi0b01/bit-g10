0ad26a32942f2430a0c1edee875bc46da71c86bb
javi <55107141+javi0b01@users.noreply.github.com>
2025-05-26 21:37:53 -0500

    workshop

--- workshops/05/js/script.js
+++ workshops/05/js/script.js
@@ -1,10 +1,30 @@
 'use strict';
+const d = document;
+
+const $root = d.getElementById('root');
+
+let cards = `
+<div class="d-flex flex-wrap">
+`;
+
 fetch('file.json')
   .then((res) => res.json())
   .then((info) => {
     for (let i = 0; i < info.length; i++) {
-      console.log(info[i].student);
+      cards += `
+<div class="card">
+  <img src="..." class="card-img-top" alt="...">
+  <div class="card-body">
+  <h5 class="card-title">${info[i].student}</h5>
+  </div>
+  <div class="card-body">
+  <a href="#" class="card-link">Card link</a>
+  </div>
+</div>
+      `;
     }
+    cards += '</div>';
+    $root.innerHTML = cards;
   })
   .catch((err) => {
     console.log('error:', err);
