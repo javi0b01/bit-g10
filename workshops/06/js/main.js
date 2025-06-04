197113c91900540dbe9d7c10e9602a7a215dbab4
javi <55107141+javi0b01@users.noreply.github.com>
2025-06-03 21:49:13 -0500

    workshop

--- workshops/06/js/main.js
+++ workshops/06/js/main.js
@@ -1,31 +1,103 @@
 'use strict';
 import { getData } from './api.js';
-import { showPokemons, showError } from './shows.js';
+import { showPokemon, showPokemons, showError } from './shows.js';
+
+let pokemons = [];
 
 document.addEventListener('DOMContentLoaded', () => {
-  getPokemons();
+  if (location.href.endsWith('products.html')) {
+    getPokemons('https://pokeapi.co/api/v2/pokemon/');
+  }
 });
 
-async function getPokemons() {
-  const res = await getData();
-  if (res.message === 'success') setPokemons(res.data.results);
-  else showError(res.message);
+document.getElementById('close').addEventListener('click', () => {
+  document.getElementById('detail').style.display = 'none';
+});
+
+window.addEventListener('click', function (event) {
+  if (event.target === this.document.getElementById('detail')) {
+    document.getElementById('detail').style.display = 'none';
+  }
+});
+
+async function getPokemons(url) {
+  const res = await getData(url);
+  if (res.message === 'success') {
+    const { next, previous } = res.data;
+    setPokemons(res.data.results);
+    setPagination({ next, previous });
+  } else showError(res.message);
 }
 
 async function getPokemon(result) {
   const res = await getData(result.url);
   if (res.message === 'success') {
     const pokemon = res.data;
-    const { name, sprites } = pokemon;
-    return { namePokemon: name, imgPokemon: sprites.front_default };
+    const {
+      id,
+      name,
+      height,
+      weight,
+      abilities,
+      base_experience,
+      types,
+      sprites,
+    } = pokemon;
+    return {
+      id,
+      name,
+      height,
+      weight,
+      abilities,
+      base_experience,
+      types,
+      sprites,
+    };
   } else showError(res.message);
 }
 
 async function setPokemons(results) {
-  const pokemons = [];
+  const arr = [];
   for (const result of results) {
     const pokemon = await getPokemon(result);
-    pokemons.push(pokemon);
+    arr.push(pokemon);
   }
+  pokemons = [...arr];
   showPokemons(pokemons);
+  watchPokemons();
+}
+
+function watchPokemons() {
+  const $pokemons = document.querySelectorAll('.pokemon');
+  $pokemons.forEach(($pokemon) => {
+    $pokemon.addEventListener('click', () => {
+      const pokemonSelected = pokemons.filter(
+        (pokemon) => pokemon.id == $pokemon.id
+      );
+      showPokemon(pokemonSelected[0]);
+    });
+  });
+}
+
+function setPagination(pagination) {
+  const $left = document.getElementById('left');
+  const $right = document.getElementById('right');
+
+  if (pagination.next) {
+    $right.classList.remove('d-none');
+  } else {
+    $right.classList.add('d-none');
+  }
+  if (pagination.previous) {
+    $left.classList.remove('d-none');
+  } else {
+    $left.classList.add('d-none');
+  }
+
+  $left.addEventListener('click', () => {
+    if (pagination.previous) getPokemons(pagination.previous);
+  });
+  $right.addEventListener('click', () => {
+    if (pagination.next) getPokemons(pagination.next);
+  });
 }
--- workshops/06/js/shows.js
+++ workshops/06/js/shows.js
@@ -1,19 +1,61 @@
 'use strict';
+export function showPokemon(pokemon) {
+  showSpinner(true);
+  const html = `
+<figure id="${pokemon.id}" class="pokemon">
+  <img src="${pokemon.sprites.front_default}" alt="Image of ${pokemon.name}">
+  <figcaption>#${pokemon.id} ${pokemon.name}</figcaption>
+</figure>
+<ul>
+  <li>Ability: ${pokemon.abilities[0].ability.name}</li>
+  <li>Base experience: ${pokemon.base_expirience}</li>
+  <li>Type: ${pokemon.types[0].type.name}</li>
+  <li>Height: ${pokemon.height}</li>
+  <li>weight: ${pokemon.weight}</li>
+</ul>
+    `;
+  const $card = document.getElementById('card');
+  $card.innerHTML = html;
+
+  setTimeout(() => {
+    showSpinner(false);
+    document.getElementById('detail').style.display = 'flex';
+  }, 750);
+}
+
 export function showPokemons(pokemons) {
-  let html = '<section>';
+  showSpinner(true);
+  let html = '';
   for (const pokemon of pokemons) {
     html += `
-<figure>
-  <img src="${pokemon.imgPokemon}" alt="Image of ${pokemon.namePokemon}">
-  <figcaption>${pokemon.namePokemon}</figcaption>
+<figure id="${pokemon.id}" class="pokemon">
+  <img src="${pokemon.sprites.back_default}" alt="Image of ${pokemon.name}">
+  <figcaption>${pokemon.name}</figcaption>
 </figure>
   `;
   }
-  html += '</section>';
-  const $main = document.querySelector('main');
-  $main.innerHTML = html;
+  const $pokemons = document.querySelector('.pokemons');
+  $pokemons.innerHTML = html;
+
+  setTimeout(() => {
+    showSpinner(false);
+  }, 1500);
 }
 
 export function showError(msg) {
   alert(msg);
+}
+
+function showSpinner(visible) {
+  const $spinner = document.getElementById('spinner');
+  const $main = document.querySelector('main');
+  if (visible) {
+    $spinner.classList.remove('d-none');
+    $spinner.classList.add('spinner');
+    $main.classList.add('d-none');
+  } else {
+    $spinner.classList.remove('spinner');
+    $spinner.classList.add('d-none');
+    $main.classList.remove('d-none');
+  }
 }
