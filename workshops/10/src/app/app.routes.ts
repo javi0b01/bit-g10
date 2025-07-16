6581f38e1fbdce2c14dac6b04234d4b159a86ae8
javi <55107141+javi0b01@users.noreply.github.com>
2025-07-15 19:09:04 -0500

    workshop

--- workshops/10/src/app/app.routes.ts
+++ workshops/10/src/app/app.routes.ts
@@ -1,4 +1,5 @@
 import { Routes } from '@angular/router';
+import { activateGuard } from './guards/activate-guard';
 import { Home } from './components/pages/home/home';
 import { Dashboard } from './components/pages/dashboard/dashboard';
 import { SignUp } from './components/pages/sign-up/sign-up';
@@ -7,7 +8,12 @@ import { PageNotFound } from './components/pages/page-not-found/page-not-found';
 
 export const routes: Routes = [
   { path: 'home', component: Home, title: 'Home | SPA' },
-  { path: 'dashboard', component: Dashboard, title: 'Dashboard | SPA' },
+  {
+    path: 'dashboard',
+    component: Dashboard,
+    title: 'Dashboard | SPA',
+    canActivate: [activateGuard],
+  },
   { path: 'sign-up', component: SignUp, title: 'Sign Up | SPA' },
   { path: 'sign-in', component: SignIn, title: 'Sign In | SPA' },
   { path: '', redirectTo: '/home', pathMatch: 'full' },
--- workshops/10/src/app/services/signin.ts
+++ workshops/10/src/app/services/signin.ts
@@ -1,4 +1,5 @@
 import { Injectable, inject } from '@angular/core';
+import { Router } from '@angular/router';
 import { HttpClient } from '@angular/common/http';
 
 @Injectable({
@@ -7,10 +8,24 @@ import { HttpClient } from '@angular/common/http';
 export class SigninService {
   constructor() {}
 
+  private router = inject(Router);
   private httpClient = inject(HttpClient);
   private apiUrl = 'http://localhost:4100/users/sign-in';
 
   loginUser(payload: any) {
     return this.httpClient.post(this.apiUrl, payload);
+  }
+
+  isUserLogged() {
+    if (localStorage.getItem('token')) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  logoutUser() {
+    localStorage.removeItem('token');
+    this.router.navigate(['/']);
   }
 }
